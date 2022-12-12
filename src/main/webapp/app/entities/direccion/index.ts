da943fd5158dd7c55f1a9d46adb2d87555e93d3b
AcostaJoaquin <AcostaJoaquin@users.noreply.github.com>
2022-12-12 14:55:55 +0100

    eliminado valoraciones imagen y direccion
    eliminado valoraciones imagen y dirección

--- src/main/webapp/app/entities/direccion/index.ts
+++ src/main/webapp/app/entities/direccion/index.ts
@@ -1,6 +1,2 @@
 export * from './direccion.service';
-export * from './direccion-update.component';
-export * from './direccion-delete-dialog.component';
-export * from './direccion-detail.component';
-export * from './direccion.component';
 export * from './direccion.route';
--- src/main/webapp/app/entities/valoraciones/valoraciones.module.ts
+++ src/main/webapp/app/entities/valoraciones/valoraciones.module.ts
@@ -4,33 +4,8 @@ import { JhiLanguageService } from 'ng-jhipster';
 import { JhiLanguageHelper } from 'app/core';
 
 import { TiendaOnlineSharedModule } from 'app/shared';
-import {
-  ValoracionesComponent,
-  ValoracionesDetailComponent,
-  ValoracionesUpdateComponent,
-  ValoracionesDeletePopupComponent,
-  ValoracionesDeleteDialogComponent,
-  valoracionesRoute,
-  valoracionesPopupRoute
-} from './';
-
-const ENTITY_STATES = [...valoracionesRoute, ...valoracionesPopupRoute];
 
 @NgModule({
-  imports: [TiendaOnlineSharedModule, RouterModule.forChild(ENTITY_STATES)],
-  declarations: [
-    ValoracionesComponent,
-    ValoracionesDetailComponent,
-    ValoracionesUpdateComponent,
-    ValoracionesDeleteDialogComponent,
-    ValoracionesDeletePopupComponent
-  ],
-  entryComponents: [
-    ValoracionesComponent,
-    ValoracionesUpdateComponent,
-    ValoracionesDeleteDialogComponent,
-    ValoracionesDeletePopupComponent
-  ],
   providers: [{ provide: JhiLanguageService, useClass: JhiLanguageService }],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
