9c4fc8cc7d73a496d872eb42adff03d03e97e852
JoseMario28 <josemarioac@hotmail.es>
2022-12-15 12:20:25 +0100

    detalles, footer

--- src/main/webapp/app/entities/producto/producto-detail.component.ts
+++ src/main/webapp/app/entities/producto/producto-detail.component.ts
@@ -5,7 +5,8 @@ import { IProducto } from 'app/shared/model/producto.model';
 
 @Component({
   selector: 'jhi-producto-detail',
-  templateUrl: './producto-detail.component.html'
+  templateUrl: './producto-detail.component.html',
+  styleUrls: ['./producto-detail.scss']
 })
 export class ProductoDetailComponent implements OnInit {
   producto: IProducto;
